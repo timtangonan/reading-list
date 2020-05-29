152690958ebc91f131dc69fff5e53bd65797b7c6
Tim Tangonan <trstangonan@gmail.com>
2020-05-29 13:30:24 +0800

    added attribution

---
+++ src/components/DevInfo.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const DevInfo = () => {
+    return(
+        <div style={{margin: '0 auto 0.8em'}}><p style={{textAlign: 'center', fontSize: '0.5rem'}}>By: <a href="https://timtangonan.com" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: '#fff' }}>Tim Tangonan</a></p></div>
+    )
+}
+
+export default DevInfo
--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,6 +1,7 @@
 import React from "react"
 import AddBook from "./AddBook"
 import BookList from "./BookList"
+import DevInfo from "./DevInfo"
 
 import "../styles/panel.css"
 
@@ -10,6 +11,7 @@ const Panel = () => {
             <div className="title-container">
                 <h1 class="app-title">Reading List <span role="img" aria-label="books">📚</span></h1>
             </div>
+            <DevInfo />
             <AddBook />
             <BookList />
         </div>
